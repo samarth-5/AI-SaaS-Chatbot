cf4ccd869164d858b27a888660bfda799a08fc2c
Samarth <kayaniv321@gmail.com>
2024-04-21 16:35:03 +0530

    added scrolling functionality

--- client/src/Components/ChatItem.jsx
+++ client/src/Components/ChatItem.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React,{useEffect,useRef} from 'react'
 import {Avatar} from 'flowbite-react';
 import ai from '../assets/logo.jpeg';
 import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter';
 import { coldarkDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 export default function ChatItem({role,content,firstLetter}) { 
+
+  const scrollRef = useRef(null);
+
+  useEffect(() => {
+    // Scroll to the bottom of the chat item component
+    if (scrollRef.current) {
+      scrollRef.current.scrollIntoView({ behavior: 'smooth', block: 'end' });
+    }
+  }, [content]);
 
   const extractCodeFromString = (message) => {
     if (message.includes("```")) {
@@ -24,7 +33,7 @@ export default function ChatItem({role,content,firstLetter}) {
     <>
       {
         role==='assistant' ? (<div className='p-2 flex gap-2 my-2 bg-[#07073d] rounded'>
-            <div style={{ alignSelf: 'flex-start' }} className='mt-2'>
+            <div style={{ alignSelf: 'flex-start' }} className='mt-2' ref={scrollRef}>
               <Avatar img={ai} alt="ai" rounded />
             </div>
             {
@@ -39,7 +48,7 @@ export default function ChatItem({role,content,firstLetter}) {
                          </SyntaxHighlighter>
             ) : (<></>)
           )}
-        </div>) : (<div className='p-2 flex gap-2 my-2 bg-[#13136e] rounded'>
+        </div>) : (<div className='p-2 flex gap-2 my-2 bg-[#13136e] rounded' ref={scrollRef}>
             <Avatar placeholderInitials={firstLetter} alt='user' rounded />
             <div className='w-full flex items-center font-sans'>{content}</div>
         </div>)
